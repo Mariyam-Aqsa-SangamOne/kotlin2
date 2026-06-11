968b21495be112bf591945e4a9b9c5378408afcb
Mariyam Aqsa Rafiq <mariyamaqsa.sangamone@gmail.com>
2026-06-11 17:08:37 +0530

    Cat sound onclick

---
+++ 1a/app/src/main/java/com/example/a1a/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.a1a
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.a1a.ui.theme._1aTheme
+import androidx.compose.foundation.Image
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.foundation.clickable
+import android.media.MediaPlayer
+import androidx.compose.foundation.layout.width
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            _1aTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    CatScreen(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun CatScreenPreview() {
+    _1aTheme {
+        CatScreen()
+    }
+}
---
+++ 1a/app/src/main/java/com/example/a1a/ScreenA.kt
@@ -0,0 +1,31 @@
+package com.example.a1a
+
+import android.media.MediaPlayer
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CatScreen(modifier: Modifier = Modifier) {
+    val context= LocalContext.current
+    val mediaPlayer= MediaPlayer.create(
+        context,
+        R.raw.meow
+    )
+    Image(
+        painter=painterResource(id=R.drawable.cat),
+        contentDescription = "Cat",
+        modifier=modifier
+            .size(250.dp)
+            .clickable{
+                mediaPlayer.start()
+            },
+        contentScale = ContentScale.Fit
+    )
+}
